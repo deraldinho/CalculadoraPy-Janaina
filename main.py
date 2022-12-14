83a1c732123ff41c3a6565f2d09ac396d386368c
Deraldo Palomino Filho <deraldinhopalomino@hotmail.com>
2022-12-14 17:24:10 -0300

    Atualização da programação

--- main.py
+++ main.py
@@ -2,67 +2,35 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-class calculadora():
-    def __init__(self):
-        self.portion1 = int()
-        self.portion2 = int()
-        self.scraps = float()
-        self.result = float()
+class Calculadora():
+    _portion1 = 0
+    _portion2 = 0
+    _scraps = 0
+    _result = 0
 
-    def __int__(self, portion1, portion2):
-        self.set_portion1(portion1)
-        self.set_portion2(portion2)
+    def portion1(self):
+        return self._portion1
 
-    def get_portion1(self):
-        return self.portion1
-
-    def get_portion1(self):
-        return self.portion2
-
-    def get_scraps(self):
-        return self.scraps
-
-    def get_result(self):
-        return self.result
-
-    def set_portion1(self, portion1):
+    def portion1(self, portion):
         try:
-            portion1 = int(portion1)
-        except ValueError:
+            portion = int(portion)
+        except:
+            print('valor incorreto')
             return False
         else:
-            self.portion1 = portion1
+            self._portion1 = portion
             return True
 
-    def set_portion2(self, portion2):
+    def portion2(self):
+        return self._portion2
+
+    def portion2(self, portion):
         try:
-            portion2 = int(portion2)
-        except ValueError:
-            return False
+            portion = int(portion)
+        except:
+            pass
         else:
-            self.portion1 = portion2
-            return True
-
-    def set_scraps(self, scraps):
-        self.scraps = scraps
-
-    def set_total(self, result):
-        self.result = result
-
-    def sum(self):
-        self.result = self.portion1 + self.portion2
-
-    def subtraction(self):
-        self.result = self.portion1 - self.portion2
-
-    def multiplication(self):
-        self.result = self.portion1 * self.portion2
-
-    def division(self):
-        self.result = self.portion1 / self.portion2
-
-    def scraps(self):
-        self.scraps = self.portion1 % self.portion2
+            self._portion2 = portion
 
 
 class Viewer():
@@ -90,7 +58,25 @@ class Viewer():
         print('4 -> Divisão')
         print('5 -> Função exponencial')
         print('0 -> Sair')
-        int(input('>>>'))
+        control = int(input('>>>'))
+        if control == 1:
+            calc = Calculadora()
+            calc.portion1(input('Entre com primeiro valor:\n>>>'))
+            calc.portion2(input('Entre com segundo valor:\n>>>'))
+            print(calc.sum())
+
+        elif control == 2:
+            pass
+        elif control == 3:
+            pass
+        elif control == 4:
+            pass
+        elif control == 5:
+            pass
+        elif control == 0:
+            pass
+        else:
+            self.counter = 0
 
     def menu_division(self):
         print('Menu de Divisão')
